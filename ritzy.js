3fd6cc20b7cc94c613838248a37f1dd9771e0a8d
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-01-30 20:45:32 +0000

    Add Features and constructors

--- ritzy.js
+++ ritzy.js
@@ -2,6 +2,24 @@
 if(typeOf jQuery == "undefined"){
   throw "jQuery is needed for ritzy to work.";
 }
+var rule = function(description,action,toAffect){
+  if(typeof toAffect !== "model"){
+    throw "To affect is not a model.";
+  }
+  if(typeof description !== "string" ){
+    throw "Description is not a string.";
+  }
+  if(typeof action !== "function"){
+    throw "Action is not function.";
+  }
+};
+var attributes =function(rules){
+  for(var i = 0; i>rules.length; i++;){
+    if(typeof rules[i] !== "rule"){
+      throw "Attribute is not rule.";
+    }
+  }
+}
 $(document).ready(function(){
   var canvasdocument = getElementsByName("canvas");
   if(canvasdocument<1){
@@ -11,9 +29,10 @@ $(document).ready(function(){
     if(typeOf name !== "string"){
       throw "name of gameController not string.";
     }
-    if(typeOf attributes !== "array"){
+    if(typeOf attributes !== "attributes"){
       throw "Attributes of gameController not a name";
     }
-    
+    this.attributes = attributes;
+    this.name = name;
   };
 });
