310f59fd46016b78c83301f15f86f59c889eae0b
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-03-08 17:52:34 +0000

    Update ritzy.js

--- ritzy.js
+++ ritzy.js
@@ -51,6 +51,15 @@ var Player = function(name,type){
   this.name = name;
   this.type = type;
 };
+var startOfTurn = function(actions){
+  if(actions){
+    for(var i=0; i<actions.length; i++){
+      actions[i].call();
+    }
+  }
+};
+var charge = function(){};
+var movementPhase = [];
 $(document).ready(function(){
   var canvasdocument = getElementsByName("canvas");
   if(canvasdocument<1){
