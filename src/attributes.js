0e430124770e5649481bdf8943fdd90a4abff33c
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-05-10 17:25:37 -0400

    Create attributes.js

---
+++ src/attributes.js
@@ -0,0 +1,10 @@
+define(["./rule"],function(rule){
+  var rule2 = rule;
+  return function(rules){		
+    for(var i = 0; i>rules.length; i++;){		
+      if(typeof rules[i] !== "rule2"){		
+        throw "Attribute is not rule.";		
+      }		
+    }		
+  };
+});
