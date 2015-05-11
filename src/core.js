800e5f9e4796c596b57bcb7a25409eaff86917ff
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-05-11 14:08:28 -0400

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,21 +1,3 @@
-var model = function(m,ws,bs,s,t,w,i,a,ld,rules,img){
-  //Defining properties
-  if(typeof rules !== array){
-    throw "Rules aren't an array";
-  }
-  this.movement = m;
-  this.ballisticSkill = bs;
-  this.weaponSkill = ws;
-  this.strength = s;
-  this.toughness = t;
-  this.wounds = w;
-  this.initiative = i;
-  this.attacks = a;
-  this.leadership = ld;
-  this.img = $.get(img,function(data){
-    this.img = data;
-  });
-};
 var gameController = function(name,attributes){
     if(typeOf name !== "string"){
       throw "name of gameController not string.";
@@ -27,4 +9,3 @@ var gameController = function(name,attributes){
     this.name = name;
 };
 
-module.exports = []
