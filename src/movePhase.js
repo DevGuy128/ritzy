f012ccbbc2d65f0f368e1e11c1334e4b3e1bf228
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-05-11 14:16:33 -0400

    Create movePhase.js

---
+++ src/movePhase.js
@@ -0,0 +1,3 @@
+define(["./startOfTurn","./charge","./compulsMoves","./remainingMoves"],function(startOfTurn,charge,compulsMoves,remainingMoves){
+  return [startOfTurn,charge,compulsMoves,remainingMoves];
+});
