decd47e27ea272bb3b9918f8e235be1ff03ad0a5
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-05-11 14:11:03 -0400

    Update ritzy.js

--- ritzy.js
+++ ritzy.js
@@ -2,13 +2,6 @@ var Player = function(name,type){
   this.name = name;
   this.type = type;
 };
-var startOfTurn = function(actions){
-  if(actions){
-    for(var i=0; i<actions.length; i++){
-      actions[i].call();
-    }
-  }
-};
 var charge = function(){
   
 };
