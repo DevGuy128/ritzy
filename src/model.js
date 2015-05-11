11bed65ae11d90f1efc556b484e8d3a6589e175a
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-05-11 14:10:13 -0400

    Create model.js

---
+++ src/model.js
@@ -0,0 +1,20 @@
+define(function(){
+  return function(m,ws,bs,s,t,w,i,a,ld,rules,img){
+    //Defining properties
+    if(typeof rules !== array){
+      throw "Rules aren't an array";
+    }
+    this.movement = m;
+    this.ballisticSkill = bs;
+    this.weaponSkill = ws;
+    this.strength = s;
+    this.toughness = t;
+    this.wounds = w;
+    this.initiative = i;
+    this.attacks = a;
+    this.leadership = ld;
+    this.img = $.get(img,function(data){
+      this.img = data;
+    });
+  };
+});
